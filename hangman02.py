682ec1b760009ccff64ff448b8ba215a7b974681
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2018-09-24 22:09:26 -0700

    Add files via upload

---
+++ hangman02.py
@@ -0,0 +1,33 @@
+'''
+word is the word player needs to guess
+
+new_word is the "guess" word, as the game starts
+At the beginning every missing letter is substituted by a '*'
+As the game progresses , the letters that are correctly discovered are
+replacing the stars'*' in the new _word
+The program asks for a new letter to be guessed as long as there is at least
+an asterisk left
+
+
+'''
+
+word = 'hangman' # Here you need to feed a new random word
+                 # from a list of words
+
+list = list(word)
+print("Now you should guess my word!")
+new_word = []
+for i in range(len(list)):
+    new_word.append("*")
+print(new_word)
+print(len(new_word))
+
+
+while '*' in new_word:
+    print("enter a letter\n\n")
+    letter = input()
+    for i in range(len(list)):
+        if list[i] == letter:
+            new_word[i] = letter
+            continue
+    print(new_word)
